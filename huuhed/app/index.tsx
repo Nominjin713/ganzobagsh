230a59810394b419ac59e31f0dff0e442c6db47d
Asaku-10 <is23d010@mandakh.edu.mn>
2026-02-10 11:04:16 +0800

    starting babydata

--- huuhed/app/index.tsx
+++ huuhed/app/index.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import {babydata} from "./data/babydata";
 export default function Page() {
+   const firstCategory = babydata[0];
   return (
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.title}>
+          {firstCategory.cname}
+        </Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
       </View>
     </View>
